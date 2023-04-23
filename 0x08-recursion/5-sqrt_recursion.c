68693b1625b562622b1ce9361a720f2b151ec1a4
notbotjustnoob <taheryassir@gmail.com>
2023-04-24 00:07:30 +0200

    Exercise 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,18 +6,18 @@
  * Return: the natural square root of n, or -1 if n does not have a natural
  * square root
  */
-int _sqrt_helper(int n, int i)
+int _sqrt_secondary(int n, int i)
 {
-    if (i * i == n)
-        return (i);
-    else if (i * i > n)
-        return (-1);
-    return (_sqrt_helper(n, i + 1));
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	return (_sqrt_secondary(n, i + 1));
 }
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    return (_sqrt_helper(n, 0));
+	if (n < 0)
+		return (-1);
+	return (_sqrt_secondary(n, 0));
 }
