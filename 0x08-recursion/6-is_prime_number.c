05c89e96da4f0683d4a058bcd9474406a04aa845
notbotjustnoob <taheryassir@gmail.com>
2023-04-24 00:17:00 +0200

    Betty style

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,10 +10,10 @@ int is_prime_number(int n)
 {
 	/* Negative numbers, 0, and 1 are not prime */
 	if (n < 2)
-		return 0;
+		return (0);
 
 	/* Check if n is divisible by any number less than n */
-	return is_prime_secondary(n, 2);
+	return (is_prime_secondary(n, 2));
 }
 
 /**
@@ -27,12 +27,12 @@ int is_prime_secondary(int n, int i)
 {
 	/* Base case: n is divisible by i, so it's not prime */
 	if (n % i == 0)
-		return 0;
+		return (0);
 
 	/* Base case: n is not divisible by any number less than i, so it's prime */
 	if (i * i > n)
-		return 1;
+		return (1);
 
 	/* Check the next divisor */
-	return is_prime_secondary(n, i + 1);
+	return (is_prime_secondary(n, i + 1));
 }
