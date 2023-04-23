973f75f656095db6167d9d215e6bfbc932a6acef
notbotjustnoob <taheryassir@gmail.com>
2023-04-24 00:49:31 +0200

    Exercise 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,44 +1,23 @@
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int is_palindrome(char *s)
-{
-    int len = _strlen_recursion(s);
-
-    if (len <= 1)
-        return (1);
-    return (_palindrome_secondary(s, 0, len - 1));
-}
+#include <stdbool.h>
+#include <string.h>
 
 /**
- * _palindrome_secondary - helper function for is_palindrome
- * @s: the string to check
- * @start: the starting index of the substring to check
- * @end: the ending index of the substring to check
+ * is_palindrome - A recursive function that checks if a string is a palindrome
+ * @s: The string to check
  *
- * Return: 1 if the substring is a palindrome, 0 otherwise
+ * Return: True if the substring is a palindrome, false otherwise
  */
-int _palindrome_secondary(char *s, int start, int end)
-{
-    if (start >= end)
-        return (1);
-    if (s[start] != s[end])
-        return (0);
-    return (_palindrome_secondary(s, start + 1, end - 1));
-}
 
-/**
- * _strlen_recursion - helper function to get the length of a string
- * @s: the string to get the length of
- *
- * Return: the length of the string
- */
-int _strlen_recursion(char *s)
+bool is_palindrome(char *s)
 {
-    if (*s == '\0')
-        return (0);
-    return (_strlen_recursion(s + 1) + 1);
+    // Base case: an empty string or a string of one character is a palindrome
+    if (strlen(s) <= 1)
+        return true;
+    
+    // Compare the first and last characters of the string
+    if (s[0] != s[strlen(s) - 1])
+        return false;
+
+    // Recursively check the substring without the first and last characters
+    return (is_palindrome(&s[1]) && is_palindrome(&s[strlen(s) - 1]));
 }
