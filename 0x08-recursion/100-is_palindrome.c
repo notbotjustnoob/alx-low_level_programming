da1eedb2615238c2c23c14612b19b0757eeb2132
notbotjustnoob <taheryassir@gmail.com>
2023-04-24 00:34:52 +0200

    Exercise 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,28 +7,22 @@
 int is_palindrome(char *s)
 {
     int len = 0;
+    char *start, *end;
 
-    while (s[len])
+    while (s[len] != '\0')
         len++;
 
-    return check_palindrome(s, 0, len - 1);
-}
+    start = s;
+    end = s + len - 1;
 
-/**
- * check_palindrome - recursive function to check if a string is a palindrome
- * @s: the string to check
- * @start: the starting index to check
- * @end: the ending index to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int check_palindrome(char *s, int start, int end)
-{
-    if (start >= end)
-        return 1;
+    while (end > start)
+    {
+        if (*start != *end)
+            return (0);
 
-    if (s[start] != s[end])
-        return 0;
+        start++;
+        end--;
+    }
 
-    return check_palindrome(s, start + 1, end - 1);
+    return (1);
 }
