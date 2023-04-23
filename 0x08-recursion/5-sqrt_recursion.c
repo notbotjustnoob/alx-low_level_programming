06b3885f448a679e860cb42dcb96f4eca58d8c10
notbotjustnoob <taheryassir@gmail.com>
2023-04-24 00:14:37 +0200

    global betty compliance

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,9 @@
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt_secondary - returns the natural square root of a number
  * @n: the number to find the square root of
  * @i: the current number to start with
  *
- * Return: the natural square root of n, or -1 if n does not have a natural
+ * Return: the natural square root of n
  * square root
  */
 int _sqrt_secondary(int n, int i)
@@ -15,6 +15,14 @@ int _sqrt_secondary(int n, int i)
 	return (_sqrt_secondary(n, i + 1));
 }
 
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the natural square root of n, or -1 if n does not have a natural
+ * square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
