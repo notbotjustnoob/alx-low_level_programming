38ac0b4a6eabf54ee282f3f61d25b9c4bc8aa6ff
notbotjustnoob <taheryassir@gmail.com>
2023-05-02 23:49:32 +0200

    Implementation betty fixes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,39 +10,39 @@
  */
 int main(int argc, char *argv[])
 {
-    int cents, coins = 0;
+	int cents, coins = 0;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-    if (cents < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    while (cents > 0)
-    {
-        if (cents >= 25)
-            cents -= 25;
-        else if (cents >= 10)
-            cents -= 10;
-        else if (cents >= 5)
-            cents -= 5;
-        else if (cents >= 2)
-            cents -= 2;
-        else
-            cents -= 1;
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else
+			cents -= 1;
 
-        coins++;
-    }
+		coins++;
+	}
 
-    printf("%d\n", coins);
+	printf("%d\n", coins);
 
-    return (0);
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,30 +10,30 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, num, sum = 0;
+	int i, num, sum = 0;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        num = atoi(argv[i]);
-        if (num == 0 && *argv[i] != '0')
-        {
-            printf("Error\n");
-            return (1);
-        }
-        else if (num < 0)
-        {
-            printf("Error\n");
-            return (1);
-        }
-        sum += num;
-    }
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+		if (num == 0 && *argv[i] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }
