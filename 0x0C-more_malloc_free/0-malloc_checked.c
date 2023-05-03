4f9afa2e2b75932dd1bbc5592556f814285db543
notbotjustnoob <taheryassir@gmail.com>
2023-05-03 22:42:23 +0200

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *p;
+	void *p;
 
-    p = malloc(b);
-    if (p == NULL)
-    {
-        exit(98);
-    }
-    return (p);
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
 }
