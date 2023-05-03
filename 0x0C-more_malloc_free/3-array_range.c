850789f02a4ddb689103089084f90daf7428d495
notbotjustnoob <taheryassir@gmail.com>
2023-05-03 22:55:42 +0200

    Task 3 betty stle compliance

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,17 +12,17 @@
 
 int *array_range(int min, int max)
 {
-    int *arr, i;
+	int *arr, i;
 
-    if (min > max)
-        return (NULL);
+	if (min > max)
+		return (NULL);
 
-    arr = malloc((max - min + 1) * sizeof(int));
-    if (arr == NULL)
-        return (NULL);
+	arr = malloc((max - min + 1) * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
 
-    for (i = 0; i <= max - min; i++)
-        arr[i] = min + i;
+	for (i = 0; i <= max - min; i++)
+		arr[i] = min + i;
 
-    return (arr);
+	return (arr);
 }
