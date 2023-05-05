13701824e7872859905428b6bebd31162a216c95
notbotjustnoob <taheryassir@gmail.com>
2023-05-05 23:27:49 +0200

    Task 1 bug fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "main.h"
+
 /**
  * print_binary - prints the binary representation of a number.
  * @n: the number to print.
@@ -7,5 +10,5 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 		print_binary(n >> 1);
 
-	_putchar((n & 1) + '0');
+	putchar((n & 1) + '0');
 }
