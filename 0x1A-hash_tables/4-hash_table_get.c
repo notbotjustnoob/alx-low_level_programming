409c021bc9868d625c59753bbdd1ec2213d997c9
notbotjustnoob <taheryassir@gmail.com>
2023-12-23 17:49:57 +0100

    BETTY

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -6,7 +6,8 @@
  * @ht: The hash table you want to look into.
  * @key: The key you are looking for.
  *
- * Return: The value associated with the element, or NULL if key couldn’t be found.
+ * Return: The value associated with the element, 
+ *          or NULL if key couldn’t be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
